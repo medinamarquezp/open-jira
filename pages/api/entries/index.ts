03a26189b502f355db5efc2f63e40e676600341e
Pedro Medina Márquez <medinamarquezp@gmail.com>
2022-09-21 22:05:11 +0200

    feat: entries index api

---
+++ pages/api/entries/index.ts
@@ -0,0 +1,21 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { getEntryList, createEntry } from "repositories/entries.repository";
+
+export default async function getEntries(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  switch (req.method) {
+    case "GET":
+      const { status } = req.query;
+      const entries = await getEntryList({ ...(status && { status }) });
+      return res.status(200).json({ entries });
+    case "POST":
+      const { content, status: newStatus } = req.body;
+      const entry = { content, status: newStatus, createdAt: Date.now() };
+      await createEntry(entry);
+      return res.status(201).json({ message: "Entry created successfully" });
+    default:
+      return res.status(404).json({ name: "Resource not found" });
+  }
+}
